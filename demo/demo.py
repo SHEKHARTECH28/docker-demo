3d6771c7ac89275eb5fba0d6613efb4dcf1cd3bb
Shekhar Saini <shekharjajam2005@gmail.com>
2026-06-15 12:42:50 +0000

    DOCKER DEMO

---
+++ demo/demo.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "Name": ["Shekhar", "Amit", "Ravi"],
+    "Age": [22, 25, 30]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
---
+++ pandas.demo/pandas-demo.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "Name": ["Amit", "Ravi", "Priya"],
+    "Age": [25, 30, 28]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
